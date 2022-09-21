78991a12af1dbc227810b938372d12af4f27f498
hridoycse197 <hridoycse.eng@gmail.com>
2022-09-21 15:53:13 +0600

    s

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,87 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:async';
+import 'dart:io';
+
+import 'package:borhanudiincollege/pages/auth/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final Completer<WebViewController> _controller =
+      Completer<WebViewController>();
+  bool check = false;
+  CookieManager? cookieManager;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    if (Platform.isAndroid) {
+      WebView.platform = SurfaceAndroidWebView();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      drawer: Drawer(
+        backgroundColor: Colors.green,
+        child: SafeArea(
+          child: Column(
+            children: [
+              GestureDetector(
+                onTap: () {},
+                child: const Card(
+                  child: ListTile(
+                    title: Text('Addmission'),
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) => LoginScreen(),
+                  ));
+                },
+                child: const Card(
+                  child: ListTile(
+                    title: Text('SignOut'),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: WebView(
+          backgroundColor: Colors.green,
+          initialUrl: 'https://www.sbpgc.edu.bd/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebViewCreated: (WebViewController webViewController) {
+            _controller.complete(webViewController);
+          },
+          onProgress: (int progress) {
+            Center(
+                child: Column(
+              children: const [
+                Text("Loading"),
+                CircularProgressIndicator(
+                  strokeWidth: 40,
+                ),
+              ],
+            ));
+            print('WebView is loading (progress : $progress%)');
+          },
+        ),
+      ),
+    );
+  }
+}
